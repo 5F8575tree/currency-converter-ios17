5c06c8a895de5f2278deda2e0d74b5a0fae5a2eb
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-03-01 14:25:10 +0900

    initial commit

--- currency-converter-ios17/ContentView.swift
+++ currency-converter-ios17/ContentView.swift
@@ -9,13 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+                // bg img
+            Image("bg-dark-purple")
+                .resizable()
+                .ignoresSafeArea()
+                .frame(height:950)
+                .frame(width:500)
+            
+            VStack {
+                Spacer()
+                // prancing pony img
+                Image("currency-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:200)
+                // currency exchange text
+                Text("How much is this?")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                // conversion section
+                HStack {
+                    // left conversions sect
+                    VStack {
+                        // currency
+                        HStack {
+                            // currency img
+                            Image(systemName: "yensign.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 24)
+                                .foregroundStyle(.white)
+                            
+                            // curency text
+                            Text("Japanese Yen")
+                                .foregroundStyle(.white)
+                        }
+                        // text field
+                        Text("text field")
+                    }
+                    // equal sign
+                    Image(systemName: "equal")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height:12)
+                        .padding([.leading, .trailing])
+                        .foregroundStyle(.white)
+                    // right conversion sec
+                    VStack {
+                        // currency
+                        HStack {
+                            // currency text
+                            Text("US Dollars")
+                                .foregroundStyle(.white)
+                            // curency img
+                            Image(systemName: "dollarsign.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 24)
+                                .foregroundStyle(.white)
+                        }
+                        // text field
+                        Text("text field")
+                    }
+                }
+                Spacer()
+                Spacer()
+                // info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.bottom, 100)
+            }
+//            .border(.yellow)
         }
-        .padding()
     }
 }
 
