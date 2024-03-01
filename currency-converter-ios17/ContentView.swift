a35c114948c09d002c92e492b6ac6127a0fa31a5
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-03-01 12:46:28 +0900

    Initial Commit

---
+++ currency-converter-ios17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  currency-converter-ios17
+//
+//  Created by Mark J Rawlins on 2024/03/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ currency-converter-ios17/currency_converter_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converter_ios17App.swift
+//  currency-converter-ios17
+//
+//  Created by Mark J Rawlins on 2024/03/01.
+//
+
+import SwiftUI
+
+@main
+struct currency_converter_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
